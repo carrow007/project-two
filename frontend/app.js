9cdcb6edf9963b3b993b3b1270e24c2e90201e3b
carrow thibault <carrow1007@gmail.com>
2016-07-20 14:04:16 -0400

    outline/little bit of code

---
+++ frontend/app.js
@@ -0,0 +1,14 @@
+window.onload = function () {
+
+
+var favButton    = document.getElementById('fav-button');
+var viewALl      = document.getElementById('view-all');
+var deleteFavs   = document.getElementById('delete-favs');
+
+
+var apiPublicKeyQuery = "&apikey=" + NEWS_API_KEY;
+
+
+
+
+}
